e8039b10b1a74f9c6d365a6505edec813c8eaa31
riyaansheth <riyaansheth@gmail.com>
2026-04-16 10:30:31 +0530

    db connection altered to pull from .env

--- src/com/expensetracker/db/DBConnection.java
+++ src/com/expensetracker/db/DBConnection.java
@@ -1,14 +1,28 @@
 package com.expensetracker.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/expense_tracker?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static String PASSWORD;
+
+    static {
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream(".env")) {
+            props.load(fis);
+            PASSWORD = props.getProperty("PASSWORD");
+        } catch (IOException e) {
+            e.printStackTrace();
+            PASSWORD = "default_password"; // fallback
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
